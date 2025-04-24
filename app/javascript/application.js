c15ec593c53f24391d83019564c6d5281d74be72
Sufii99 <sufianfaris@gmail.com>
2025-04-24 19:53:12 +0200

    Añadido: Alertas flotantes para inicio de sesión

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "./controllers"
 import './mobile_menu.js';
 import './header_transition.js';
 import './help_drop.js';
+import './notifications.js';
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
@@ -11,13 +12,13 @@ import BarChart from "./components/BarChart";
 document.addEventListener("turbo:load", () => {
   const reactRoot = document.getElementById("react-root");
   if (reactRoot) {
-    const root = createRoot(reactRoot);  // Usar createRoot en lugar de render
+    const root = createRoot(reactRoot);  
     root.render(<App />);
   }
 
   const d3Container = document.getElementById("d3-container");
   if (d3Container) {
-    const rootD3 = createRoot(d3Container);  // También usar createRoot aquí
+    const rootD3 = createRoot(d3Container); 
     rootD3.render(<BarChart />);
   }
 });
