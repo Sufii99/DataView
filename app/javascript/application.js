a5d6461a7ee8df0b8355397e57b12687ba2535a7
Sufii99 <sufianfaris@gmail.com>
2025-04-25 17:31:46 +0200

    Añadido: Idioma español para las alertas

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,8 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
-import './mobile_menu.js';
-import './header_transition.js';
-import './help_drop.js';
-import './notifications.js';
+import './header/mobile_menu.js';
+import './header/header_transition.js';
+import './header/help_drop.js';
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
