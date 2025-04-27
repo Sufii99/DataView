a246dec9a99df0f5ce3b317407dabd5edcc6fd3c
Sufii99 <sufianfaris@gmail.com>
2025-04-27 10:47:28 +0200

    Corregido: todos los errores en test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,17 +5,10 @@ import './header/header_transition.js';
 import './header/help_drop.js';
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App'
 import BarChart from "./components/BarChart";
 import DashboardApp from './components/DashboardApp'
 
 document.addEventListener("turbo:load", () => {
-  const reactRoot = document.getElementById("react-root");
-  if (reactRoot) {
-    const root = createRoot(reactRoot);  
-    root.render(<App />);
-  }
-
   const d3Container = document.getElementById("d3-container");
   if (d3Container) {
     const rootD3 = createRoot(d3Container); 
