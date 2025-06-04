668b4f1d77a03861548081ca02e47ce6902bf2a7
Sufii99 <sufianfaris@gmail.com>
2025-06-04 16:15:23 +0200

    Actualizado: Añadido usuarios de ejemplo, normal y administrador

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creando usuarios de ejemplo..."
+
+User.find_or_create_by!(email: "usuario@dataview.com") do |user|
+  user.password = "123456"
+  user.password_confirmation = "123456"
+  user.first_name = "Usuario"
+  user.last_name = "Ejemplo"
+  user.confirmed_at = Time.now
+  user.country = "Estados Unidos"
+end
+
+User.find_or_create_by!(email: "admin@dataview.com") do |user|
+  user.password = "123456"
+  user.password_confirmation = "123456"
+  user.first_name = "Administrador"
+  user.last_name = "Ejemplo"
+  user.admin = true
+  user.confirmed_at = Time.now
+  user.country = "España"
+end
+
+puts "Usuarios creados:"
+puts "Email: usuario@dataview.com | Password: 123456"
+puts "Email: admin@dataview.com   | Password: 123456"
